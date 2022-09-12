2bdf02c74eeeaf995d34679e0ec6ce468cbea209
roshnins <roshnins@hotmail.com>
2022-09-12 23:58:21 +0800

    Adding Header and FeedBackList components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
+import { useState } from "react";
 import React from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my Blog post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  const loading = false;
-  const showComments = true;
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
-  /* conditionals */
-  if (loading) return <h1>Loading...</h1>;
-
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 // Main component in root
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,
-             document.getElementById("root"));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
