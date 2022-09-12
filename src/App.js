bda6b3a11d807691d1d53201451217ec2df6a7a7
roshnins <roshnins@hotmail.com>
2022-09-12 21:49:55 +0800

    JSX good to know

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import React from "react";
+
 function App() {
-    return <h1>Hello from the app component</h1>
+  const title = "Blog Post";
+  const body = "This is my Blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  /* conditionals */
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+    </div>
+  );
 }
 
-export default App
+export default App;
