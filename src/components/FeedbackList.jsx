429ddf33134ac3c3c3988a5ac70f32ec21898209
roshnins <roshnins@hotmail.com>
2022-09-13 21:49:37 +0800

    Adding animation to FeedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
 
@@ -6,6 +7,29 @@ function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
+
+  return (
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
+
+  /* without animation 
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
@@ -13,6 +37,7 @@ function FeedbackList({ feedback, handleDelete }) {
       ))}
     </div>
   );
+  */
 }
 
 FeedbackList.propTypes = {
